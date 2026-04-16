df7b2ec6fdf10df02aeb20bc4e867854b27bb107
ael3ttar14 <ahmed.el3ttar14@example.com>
2026-04-16 18:13:17 +0200

    products

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import { getStorage } from "firebase/storage";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyADjmnAO27aGCGiNcpOKlW-LU_yarBLyrI",
-  authDomain: "attar-hub.firebaseapp.com",
-  projectId: "attar-hub",
-  storageBucket: "attar-hub.appspot.com",
-  messagingSenderId: "463116358273",
-  appId: "1:463116358273:web:c9a55d779bbc93a2bdb380",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
