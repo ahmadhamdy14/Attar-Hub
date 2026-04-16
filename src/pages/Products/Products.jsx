b1efe5cd4f795d7764a3af4c004d3b55a5ffdc0c
ael3ttar14 <ahmed.el3ttar14@example.com>
2026-04-16 18:53:17 +0200

    p

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -3,15 +3,16 @@ import { db } from "../../firebase";
 import { collection, getDocs } from "firebase/firestore";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext";
+import "./Products.css";
 
 const Products = () => {
-  const [Products, setProducts] = useState([]);
+  const [products, setProducts] = useState([]);
   const { userData } = useContext(AuthContext);
 
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const snapshot = await getDocs(collection(db, "Products"));
+        const snapshot = await getDocs(collection(db, "products"));
 
         const data = snapshot.docs.map((doc) => ({
           id: doc.id,
@@ -28,13 +29,13 @@ const Products = () => {
   }, []);
 
   return (
-    <div className="Products">
+    <div className="products">
       <h1>🛍️ Products</h1>
 
-      <div className="Products-grid">
+      <div className="products-grid">
 
-        {/* Products */}
-        {Products.map((product) => (
+        {/* PRODUCTS */}
+        {products.map((product) => (
           <div className="card" key={product.id}>
             <img src={product.image} alt={product.name} />
 
@@ -53,9 +54,9 @@ const Products = () => {
         {userData?.role === "admin" && (
           <Link to="/add-product" className="card add-card">
             <div className="card-body add-card-body">
-              <h3>Add Product</h3>
               <span className="plus">+</span>
-            
+              <h3>Add Product</h3>
+              <p>Click to add new product</p>
             </div>
           </Link>
         )}
